0d24228b71e443828fa394565e4a3696a05cca26
freaky4wrld <freaky4wrld@gmail.com>
2023-08-25 14:22:08 +0530

    created the right component as well along with mobile responsiveness, left-- drop shadow and remove margin in mobile screen

--- results-summary-component-main/data.js
+++ results-summary-component-main/data.js
@@ -2,21 +2,31 @@ const dataObjectArray = [
   {
     "category": "Reaction",
     "score": 80,
-    "icon": "./assets/images/icon-reaction.svg"
+    "icon": "../assets/images/icon-reaction.svg"
   },
   {
     "category": "Memory",
     "score": 92,
-    "icon": "./assets/images/icon-memory.svg"
+    "icon": "../assets/images/icon-memory.svg"
   },
   {
     "category": "Verbal",
     "score": 61,
-    "icon": "./assets/images/icon-verbal.svg"
+    "icon": "../assets/images/icon-verbal.svg"
   },
   {
     "category": "Visual",
     "score": 72,
-    "icon": "./assets/images/icon-visual.svg"
+    "icon": "../assets/images/icon-visual.svg"
   }
 ];
+
+const resultElements = document.querySelectorAll('.result-element');
+const resultElementsArray = [];
+resultElements.forEach((element)=>{
+  resultElementsArray.push(element)
+  let elementPosition = resultElementsArray.indexOf(element)
+  element.children[0].children[1].children[0].innerText = dataObjectArray[elementPosition].score
+  element.children[0].children[0].children[0].src  = dataObjectArray[elementPosition].icon
+  element.children[0].children[0].children[1].innerText = dataObjectArray[elementPosition].category
+})
