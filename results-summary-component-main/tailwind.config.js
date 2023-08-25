116590e4f337bba340d91702852d69ba13b99d94
freaky4wrld <freaky4wrld@gmail.com>
2023-08-25 12:41:53 +0530

    created the result left component with responsiveness

--- results-summary-component-main/tailwind.config.js
+++ results-summary-component-main/tailwind.config.js
@@ -18,12 +18,15 @@ module.exports = {
         lightSlateBlue: 'hsl(252, 100%, 67%)', //background
         lightRoyalBlue: 'hsl(241, 81%, 54%)', //background
         violetBlue: 'hsla(256, 72%, 46%, 1)', //circle
-        PersianBlue: 'hsla(241, 72%, 46%, 0', //circle
+        persianBlue: 'hsla(241, 72%, 46%, 0)', //circle
     
         white: 'hsl(0, 0%, 100%)',
         paleBlue: 'hsl(221, 100%, 96%)',
         lightLavender: 'hsl(241, 100%, 89%)',
         darkGrayBlue: 'hsl(224, 30%, 27%)',
+    },
+    fontFamily: {
+      'sans': ['hanken', 'sans-serif'],
     }
     },
   },
