6ef761cdc067023dc54db411aa6be14e342c7797
freaky4wrld <freaky4wrld@gmail.com>
2023-08-26 13:00:08 +0530

    setup tailwind functionality with the project

--- testimonials-grid-section-main/tailwind.config.js
+++ testimonials-grid-section-main/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/index.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
   },
   plugins: [],
 }
