2d51361c79f41a671a04656717ac7b901dfa026f
freaky4wrld <freaky4wrld@gmail.com>
2023-08-26 12:06:22 +0530

    created the structure and styled the element, also added responsiveness to the page

--- four-card-feature-section-master/tailwind.config.js
+++ four-card-feature-section-master/tailwind.config.js
@@ -13,10 +13,11 @@ module.exports = {
         'sans': ['Poppins', 'sans-serif'],
       },
       colors:{
-        red: 'hsl(0, 78%, 62%)',
-        cyan: 'hsl(180, 62%, 55%)',
-        orange: 'hsl(34, 97%, 64%)',
-        blue: 'hsl(212, 86%, 64%)',
+        Red: 'hsl(0, 78%, 62%)',
+        Cyan: 'hsl(180, 62%, 55%)',
+        Orange: 'hsl(34, 97%, 64%)',
+        Blue: 'hsl(212, 86%, 64%)',
+        //Neutral
         veryDarkBlue: 'hsl(234, 12%, 34%)',
         grayishBlue: 'hsl(229, 6%, 66%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
