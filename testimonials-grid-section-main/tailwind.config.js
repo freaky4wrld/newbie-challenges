876df1073ee359925e5b8d2b0605efc51e4d1c9a
freaky4wrld <freaky4wrld@gmail.com>
2023-08-26 13:12:33 +0530

    added font to tailwind and also added custom color as well

--- testimonials-grid-section-main/tailwind.config.js
+++ testimonials-grid-section-main/tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
+    extend:{
+      fontFamily: {
+        'sans': ['Barlow Semi Condensed', 'sans-serif'],
+      },
+      colors: {
+        moderateViolet: 'hsl(263, 55%, 52%)',
+        veryDarkGrayishBlue: 'hsl(217, 19%, 35%)',
+        veryDarkBlackishBlue: 'hsl(219, 29%, 14%)',
+        White: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(0, 0%, 81%)',
+        lightGrayishBlue: 'hsl(210, 46%, 95%)',
+      }
+    },
   },
   plugins: [],
 }
