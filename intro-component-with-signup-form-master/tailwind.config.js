accc1a750160a4b5383a521743fb80f63f7fde6d
freaky4wrld <freaky4wrld@gmail.com>
2023-08-27 10:19:19 +0530

    successfully installed tailwindcss with npm and configured it with screen sizes, font-family and custom colors...

--- intro-component-with-signup-form-master/tailwind.config.js
+++ intro-component-with-signup-form-master/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/index.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        Red: 'hsl(0, 100%, 74%) ',
+        Green: 'hsl(154, 59%, 51%)',
+        Blue: 'hsl(248, 32%, 49%)',
+        darkBlue: 'hsl(249, 10%, 26%) ',
+        grayishBlue: 'hsl(246, 25%, 77%)',
+      }
+    },
   },
   plugins: [],
 }
